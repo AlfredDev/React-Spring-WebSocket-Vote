69175226640766f3e6ccf11ba13d3c38a6e97c84
AlfredDev <alfredgarcia14200@gmail.com>
2024-08-08 21:58:35 -0600

    Finishing controllers

--- Vote/target/generated-sources/annotations/com/websocket/voteApp/Vote/Mappers/CandidateMapperImpl.java
+++ Vote/target/generated-sources/annotations/com/websocket/voteApp/Vote/Mappers/CandidateMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-07T00:20:34-0600",
+    date = "2024-08-08T21:56:38-0600",
     comments = "version: 1.5.0.Final, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
 )
 @Component
--- Vote/target/generated-sources/annotations/com/websocket/voteApp/Vote/Mappers/PollMapperImpl.java
+++ Vote/target/generated-sources/annotations/com/websocket/voteApp/Vote/Mappers/PollMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-08T14:03:12-0600",
+    date = "2024-08-08T21:56:37-0600",
     comments = "version: 1.5.0.Final, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
 )
 @Component
