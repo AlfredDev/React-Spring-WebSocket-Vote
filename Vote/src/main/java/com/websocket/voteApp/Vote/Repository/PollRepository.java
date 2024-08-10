b7daff8a90dd10e062dc3a6d6506d143a990cfe8
AlfredDev <alfredgarcia14200@gmail.com>
2024-08-10 16:51:23 -0600

    Testing CI/CD with Docker Posgrest

--- Vote/src/main/java/com/websocket/voteApp/Vote/Repository/PollRepository.java
+++ Vote/src/main/java/com/websocket/voteApp/Vote/Repository/PollRepository.java
@@ -2,6 +2,8 @@ package com.websocket.voteApp.Vote.Repository;
 
 import com.websocket.voteApp.Vote.Models.Poll;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PollRepository extends JpaRepository<Poll, Long> {
 }
